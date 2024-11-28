c61cadb5dc40ed9935ae9fb5763b7d749e336535
Solo <40409775+Hadimhz@users.noreply.github.com>
2024-11-28 19:09:07 +0200

    Update Icon.java

--- supervisor-bukkit-item/src/main/java/com/vertmix/supervisor/core/bukkit/item/Icon.java
+++ supervisor-bukkit-item/src/main/java/com/vertmix/supervisor/core/bukkit/item/Icon.java
@@ -1,11 +1,54 @@
 package com.vertmix.supervisor.core.bukkit.item;
 
+import lombok.AllArgsConstructor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
-public class Icon {// todo... make this classx
+import java.util.List;
 
-    public String test = "material";
+@AllArgsConstructor
+public class Icon {
 
-    public Icon() {}
+    public Material material;
+    public int amount = 0;
+    public String name;
+    public List<String> lore;
+    public int customModelData = 0;
+
+    private transient ItemStack itemStack;
+
+    public Icon(Material material, int amount) {
+        this.material = material;
+        this.amount = amount;
+    }
+
+    public Icon(Material material, int amount, String name, List<String> lore) {
+        this.material = material;
+        this.amount = amount;
+        this.name = name;
+        this.lore = lore;
+    }
+
+    public ItemStack getItemstack() {
+
+        if (itemStack == null) {
+            itemStack = new ItemStack(material, amount);
+
+            itemStack.editMeta(meta -> {
+
+                if (name != null && !name.isEmpty())
+                    meta.displayName(Text.translate(name));
+
+                if (lore != null && !lore.isEmpty())
+                    meta.lore(Text.translate(lore));
+
+                meta.setCustomModelData(0);
+            });
+
+        }
+
+        return itemStack.clone();
+
+    }
+
 }
